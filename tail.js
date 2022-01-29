f7963c264319e3c1e1402b85dd2411743d7c4476
Brenda <brendamvreal@gmail.com>
2022-01-29 16:58:40 -0500

    Added new file

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+function tail(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+// tail - function reference
+// tail() - function call without parameters
+// tail([1,3]) - function call with parameters/arguments
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
