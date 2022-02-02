29bbd89560ad8a14d985d5ee4c291babf0812b61
Brenda <brendamvreal@gmail.com>
2022-02-02 00:28:36 -0500

    Fixed bugs and  implemented testing

--- without.js
+++ without.js
@@ -1,43 +1,52 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i] && arr1.length === arr2.length) {
-      return true;
-    }
-  }
-  return false;
-};
 const assertArraysEqual = function(arr1, arr2) {
-  // 1) make  sure item 1 and 2 are even comparable (object type, size etc.)
-  if (arr1.length !== arr2.length) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
     console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return;
   }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }  
   for (let i = 0; i < arr1.length; i++) {
-    // 2) loop through the elements in the items and if we find a diff, return/print that the objects are not the same and exit the loop.
     if (arr1[i] !== arr2[i]) {
-      console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-      return;
+      return false;
     }
   }
-  console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return;
+  return true;
 };
+
 const without = function(source, itemsToRemove) {
-  let newArray = [];
+  const newArray = [];
   for (let i = 0; i < source.length; i++) {
-    if (!itemsToRemove.includes(source[i])) { 
-      newArray.push(source[i]);
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        break;
+      }
+      if (j == itemsToRemove.length - 1) {
+        newArray.push(source[i]);
+      }
     }
   }
   return newArray;
 };
-assertEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]), true);
-console.log(without([1, 2, 3], [1]));
-console.log(without(['1', '2', '3'], [1, 2, '3']));
+
+// Make sure the original array was not altered by the without function
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+const numberArr = [1, 2, 3];
+const newNumberArr = without(numberArr, [3]);
+assertArraysEqual(newNumberArr, [1,2]);
+
+const nameArr = ["paul", "pedro", "paulo"];
+const newNameArr = without(nameArr, ["patricia"]);
+assertArraysEqual(newNameArr, nameArr);
+
+const listOfItems = ["1", "2", "3"];
+const newListOfItems = without(listOfItems, [1, 2, "3"]);
+assertArraysEqual(newListOfItems, [2, 3]);
+assertArraysEqual(newListOfItems, ["1", "2"]); 
