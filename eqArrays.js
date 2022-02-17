0acf96fb7492f1c51bd8436e46f7d2da682c11e6
Brenda <brendamvreal@gmail.com>
2022-02-17 08:02:17 -0500

    Refactored lotide adding testing modules and testing files

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] === arr2[i] && arr1.length === arr2.length) {
@@ -14,9 +7,5 @@ const eqArrays = function(arr1, arr2) {
   return false;
 };
 
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [2, 1, 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
--- test/headTest.js
+++ test/headTest.js
@@ -4,4 +4,3 @@ const assertEqual = require('../assertEqual');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7,8,9,10]), 2,5);
