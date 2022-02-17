57eb2e934d9acbdf9a08c5e07ed90c31711f398f
Brenda <brendamvreal@gmail.com>
2022-02-17 08:41:52 -0500

    Refactored eqArrays to re-use for asserArraysEqual, added file for testing and testing modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,30 @@
 const eqArrays = function(arr1, arr2) {
+  let output = true;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i] && arr1.length === arr2.length) {
-      return true;
+    if (Array.isArray(arr1[i]) || Array.isArray(arr2[i])) {
+      output = output && eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) {
+      output = output && false;
     }
   }
-  return false;
+  return output;
 };
 
 module.exports = eqArrays;
 
+/* if (arr1.length !== arr2.length) {
+  console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  return;
+}
+for (let i = 0; i < arr1.length; i++) {
+  // 2) loop through the elements in the items and if we find a diff, return/print that the objects are not the same and exit the loop.
+  if (arr1[i] !== arr2[i]) {
+    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    return;
+  }
+}
+console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+return; */
